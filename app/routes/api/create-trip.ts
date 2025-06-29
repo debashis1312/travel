3ea41e1a9f3756d665186926dc028454a4ecb200
debashis1312 <debashispaul1312@gmail.com>
2025-06-29 18:11:17 +0530

    final

--- app/routes/api/create-trip.ts
+++ app/routes/api/create-trip.ts
@@ -1,229 +1,118 @@
-import {Header} from "../../../components";
-import {ComboBoxComponent} from "@syncfusion/ej2-react-dropdowns";
-import type { Route } from './+types/create-trip'
-import {comboBoxItems, selectItems} from "~/constants";
-import {cn, formatKey} from "~/lib/utils";
-import {LayerDirective, LayersDirective, MapsComponent} from "@syncfusion/ej2-react-maps";
-import React, {useState} from "react";
-import {world_map} from "~/constants/world_map";
-import {ButtonComponent} from "@syncfusion/ej2-react-buttons";
-import {account} from "~/appwrite/client";
-import {useNavigate} from "react-router";
+import {type ActionFunctionArgs, data} from "react-router";
+import {GoogleGenerativeAI} from "@google/generative-ai";
+import {parseMarkdownToJson, parseTripData} from "~/lib/utils";
+import {appwriteConfig, database} from "~/appwrite/client";
+import {ID} from "appwrite";
+import {createProduct} from "~/lib/stripe";
 
-export const loader = async () => {
-    const response = await fetch('https://restcountries.com/v3.1/all');
-    const data = await response.json();
+export const action = async ({ request }: ActionFunctionArgs) => {
+    const {
+        country,
+        numberOfDays,
+        travelStyle,
+        interests,
+        budget,
+        groupType,
+        userId,
+    } = await request.json();
 
-    return data.map((country: any) => ({
-        name: country.flag + country.name.common,
-        coordinates: country.latlng,
-        value: country.name.common,
-        openStreetMap: country.maps?.openStreetMap,
-    }))
-}
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+    const unsplashApiKey = process.env.UNSPLASH_ACCESS_KEY!;
 
-const CreateTrip = ({ loaderData }: Route.ComponentProps ) => {
-    const countries = loaderData as Country[];
-    const navigate = useNavigate();
-
-    const [formData, setFormData] = useState<TripFormData>({
-        country: countries[0]?.name || '',
-        travelStyle: '',
-        interest: '',
-        budget: '',
-        duration: 0,
-        groupType: ''
-    });
-    const [error, setError] = useState<string | null>(null);
-    const [loading, setLoading] = useState(false);
-
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-       e.preventDefault()
-        setLoading(true);
-
-       if(
-           !formData.country ||
-           !formData.travelStyle ||
-           !formData.interest ||
-           !formData.budget ||
-           !formData.groupType
-       ) {
-           setError('Please provide values for all fields');
-           setLoading(false)
-           return;
-       }
-
-       if(formData.duration < 1 || formData.duration > 10) {
-           setError('Duration must be between 1 and 10 days');
-           setLoading(false)
-           return;
-       }
-       const user = await account.get();
-       if(!user.$id) {
-           console.error('User not authenticated');
-           setLoading(false)
-           return;
-       }
-
-       try {
-           const response = await fetch('/api/create-trip', {
-               method: 'POST',
-               headers: { 'Content-Type': 'application/json'},
-               body: JSON.stringify({
-                   country: formData.country,
-                   numberOfDays: formData.duration,
-                   travelStyle: formData.travelStyle,
-                   interests: formData.interest,
-                   budget: formData.budget,
-                   groupType: formData.groupType,
-                   userId: user.$id
-               })
-           })
-
-           const result: CreateTripResponse = await response.json();
-
-           if(result?.id) navigate(`/trips/${result.id}`)
-           else console.error('Failed to generate a trip')
-       } catch (e) {
-           console.error('Error generating trip', e);
-       } finally {
-           setLoading(false)
-       }
-    };
-
-    const handleChange = (key: keyof TripFormData, value: string | number)  => {
-    setFormData({ ...formData, [key]: value})
-    }
-    const countryData = countries.map((country) => ({
-        text: country.name,
-        value: country.value,
-    }))
-
-    const mapData = [
+    try {
+        const prompt = `Generate a ${numberOfDays}-day travel itinerary for ${country} based on the following user information:
+        Budget: '${budget}'
+        Interests: '${interests}'
+        TravelStyle: '${travelStyle}'
+        GroupType: '${groupType}'
+        Return the itinerary and lowest estimated price in a clean, non-markdown JSON format with the following structure:
         {
-            country: formData.country,
-            color: '#EA382E',
-            coordinates: countries.find((c: Country) => c.name === formData.country)?.coordinates || []
-        }
-    ]
+        "name": "A descriptive title for the trip",
+        "description": "A brief description of the trip and its highlights not exceeding 100 words",
+        "estimatedPrice": "Lowest average price for the trip in USD, e.g.$price",
+        "duration": ${numberOfDays},
+        "budget": "${budget}",
+        "travelStyle": "${travelStyle}",
+        "country": "${country}",
+        "interests": ${interests},
+        "groupType": "${groupType}",
+        "bestTimeToVisit": [
+          '🌸 Season (from month to month): reason to visit',
+          '☀️ Season (from month to month): reason to visit',
+          '🍁 Season (from month to month): reason to visit',
+          '❄️ Season (from month to month): reason to visit'
+        ],
+        "weatherInfo": [
+          '☀️ Season: temperature range in Celsius (temperature range in Fahrenheit)',
+          '🌦️ Season: temperature range in Celsius (temperature range in Fahrenheit)',
+          '🌧️ Season: temperature range in Celsius (temperature range in Fahrenheit)',
+          '❄️ Season: temperature range in Celsius (temperature range in Fahrenheit)'
+        ],
+        "location": {
+          "city": "name of the city or region",
+          "coordinates": [latitude, longitude],
+          "openStreetMap": "link to open street map"
+        },
+        "itinerary": [
+        {
+          "day": 1,
+          "location": "City/Region Name",
+          "activities": [
+            {"time": "Morning", "description": "🏰 Visit the local historic castle and enjoy a scenic walk"},
+            {"time": "Afternoon", "description": "🖼️ Explore a famous art museum with a guided tour"},
+            {"time": "Evening", "description": "🍷 Dine at a rooftop restaurant with local wine"}
+          ]
+        },
+        ...
+        ]
+    }`;
 
-    return (
-        <main className="flex flex-col gap-10 pb-20 wrapper">
-            <Header title="Add a New Trip" description="View and edit AI Generated travel plans" />
+        const textResult = await genAI
+            .getGenerativeModel({ model: 'gemini-2.0-flash' })
+            .generateContent([prompt])
 
-            <section className="mt-2.5 wrapper-md">
-                <form className="trip-form" onSubmit={handleSubmit}>
-                    <div>
-                        <label htmlFor="country">
-                            Country
-                        </label>
-                        <ComboBoxComponent
-                            id="country"
-                            dataSource={countryData}
-                            fields={{ text: 'text', value: 'value' }}
-                            placeholder="Select a Country"
-                            className="combo-box"
-                            change={(e: { value: string | undefined }) => {
-                                if(e.value) {
-                                    handleChange('country', e.value)
-                                }
-                            }}
-                            allowFiltering
-                            filtering={(e) => {
-                                const query = e.text.toLowerCase();
+        const trip = parseMarkdownToJson(textResult.response.text());
 
-                                e.updateData(
-                                    countries.filter((country) => country.name.toLowerCase().includes(query)).map(((country) => ({
-                                        text: country.name,
-                                        value: country.value
-                                    })))
-                                )
-                            }}
-                        />
-                    </div>
+        const imageResponse = await fetch(
+            `https://api.unsplash.com/search/photos?query=${country} ${interests} ${travelStyle}&client_id=${unsplashApiKey}`
+        );
 
-                    <div>
-                        <label htmlFor="duration">Duration</label>
-                        <input
-                            id="duration"
-                            name="duration"
-                            type="number"
-                            placeholder="Enter a number of days"
-                            className="form-input placeholder:text-gray-100"
-                            onChange={(e) => handleChange('duration', Number(e.target.value))}
-                        />
-                    </div>
+        const imageUrls = (await imageResponse.json()).results.slice(0, 3)
+            .map((result: any) => result.urls?.regular || null);
 
-                    {selectItems.map((key) => (
-                        <div key={key}>
-                            <label htmlFor={key}>{formatKey(key)}</label>
+        const result = await database.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.tripCollectionId,
+            ID.unique(),
+            {
+                tripDetails: JSON.stringify(trip),
+                createdAt: new Date().toISOString(),
+                imageUrls,
+                userId,
+            }
+        )
 
-                            <ComboBoxComponent
-                                id={key}
-                                dataSource={comboBoxItems[key].map((item) => ({
-                                    text: item,
-                                    value: item,
-                                }))}
-                                fields={{ text: 'text', value: 'value'}}
-                                placeholder={`Select ${formatKey(key)}`}
-                                change={(e: { value: string | undefined }) => {
-                                    if(e.value) {
-                                        handleChange(key, e.value)
-                                    }
-                                }}
-                                allowFiltering
-                                filtering={(e) => {
-                                    const query = e.text.toLowerCase();
+        const tripDetail = parseTripData(result.tripDetails) as Trip;
+        const tripPrice = parseInt(tripDetail.estimatedPrice.replace('$', ''), 10)
+        const paymentLink = await createProduct(
+            tripDetail.name,
+            tripDetail.description,
+            imageUrls,
+            tripPrice,
+            result.$id
+        )
 
-                                    e.updateData(
-                                        comboBoxItems[key]
-                                            .filter((item) => item.toLowerCase().includes(query))
-                                            .map(((item) => ({
-                                                text: item,
-                                                value: item,
-                                            }))))}}
-                                className="combo-box"
-                            />
-                        </div>
-                    ))}
+        await database.updateDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.tripCollectionId,
+            result.$id,
+            {
+                payment_link: paymentLink.url
+            }
+        )
 
-                    <div>
-                        <label htmlFor="location">
-                            Location on the world map
-                        </label>
-                        <MapsComponent>
-                            <LayersDirective>
-                                <LayerDirective
-                                    shapeData={world_map}
-                                    dataSource={mapData}
-                                    shapePropertyPath="name"
-                                    shapeDataPath="country"
-                                    shapeSettings={{ colorValuePath: "color", fill: "#E5E5E5" }}
-                                />
-                            </LayersDirective>
-                        </MapsComponent>
-                    </div>
-
-                    <div className="bg-gray-200 h-px w-full" />
-
-                    {error && (
-                        <div className="error">
-                            <p>{error}</p>
-                        </div>
-                    )}
-                    <footer className="px-6 w-full">
-                        <ButtonComponent type="submit"
-                            className="button-class !h-12 !w-full" disabled={loading}
-                        >
-                            <img src={`/assets/icons/${loading ? 'loader.svg' : 'magic-star.svg'}`} className={cn("size-5", {'animate-spin': loading})} />
-                            <span className="p-16-semibold text-white">
-                                {loading ? 'Generating...' : 'Generate Trip'}
-                            </span>
-                        </ButtonComponent>
-                    </footer>
-                </form>
-            </section>
-        </main>
-    )
+        return data({ id: result.$id })
+    } catch (e) {
+        console.error('Error generating travel plan: ', e);
+    }
 }
-export default CreateTrip
